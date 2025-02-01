f4e8950c92abdea92e22f3ae6c2d394b2e278ee9
ayush-bisen <ayushbisen610@gmail.com>
2025-02-02 01:42:21 +0530

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,15 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+ {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'], // Replace * with your frontend URL for better security
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
